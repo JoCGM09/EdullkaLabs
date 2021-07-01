1d877cba62854203f3f6c847061f64e14fcd45d6
José Guerra <72484685+JoCGM09@users.noreply.github.com>
2021-07-01 22:01:18 +0000

    Carpetas

--- main.py
+++ main.py
@@ -1,14 +1,77 @@
-#José Guerra
-#a
-#Cristopher
+##### ejercicios generales ######
+##### para reubicar #####
 
-def hola_mundo():
-  print("Hola mundo")
+## Sesión 1:
+# 1. Capacidades de Python
+# 2. Áreas para usar Python
+# 3. ¿Qué puedo crear con Python? Proyectos
+# 4. ¿Por qué aprender cosas básicas? Matemáticas y programación
+# ## Sesión 2:
+# 1. Tipos de datos 1(Booleanos, enteros, float...) bytes
+# 2. Input and output
+# 3. Ascii
+# ## Sesión 3:
+# 1. Operadores
+# 2. Flujos de ejecución (diagramas de flujo)
+# ## Sesión 4:
+# 1. Condicionales (if, else, elif)
+# ## Sesión 5:
+# 1. Bucle For
+# ## Sesión 6:
+# 1. While y do while
+# ## Sesión 7:
+# 1. Switch
+# ## Sesión 8:
+# 1. Tipos de datos 2(arreglos)
+# 2. Métodos de arreglos
+# ## Sesión 9:
+# 1. Listas, tuplas y diccionarios
+# 2. Métodos de 1.
+# ## Sesión 10:
+# 1. Funciones
+# ## Sesión 11:
+# 1. Buenas prácticas de programación
+# ## Sesión 12:
+# 1. Test o competencia
 
-def suma(a,b):
-  return a+b
 
-print(suma(3,6))
+##### Ejercicios #####
 
-
-#holaaa 
+# miles to km 
+# area de un triángulo
+# resolver ecuación cuadrática
+# cambiar 2 variables
+# calculadora de interés simple
+# número más largo en una lista
+# verificar si es primo
+# factorial
+# Fibonacci
+# Números Armstrong o similares
+# números divisibles
+# generar números aleatorios
+# Celsius a Fahrenheit
+# encontrar el más grande
+# Calculadora de años biciestos
+# todos los primos en un intervalo
+# verificar número positivo
+# MCM
+# MCD
+# verificar palíndromo
+# contador de vocales
+# letras repetidas
+# figuras con los *
+# Factores de un número
+# suma de todos los dígitos
+# reverse string
+# función signo
+# concatenar listas o arreglos
+# verificar si hay números en un string
+# contar los elementos repetidos de un array
+# Mostrar calendario
+# suma de naturales
+# matriz
+# decimal a binario
+# imprimir números en un intervalo
+# mostrar tabla de multiplicar
+# función escalón unitario
+# calculadora simple
